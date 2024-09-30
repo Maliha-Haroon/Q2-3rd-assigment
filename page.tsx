c1a36cd2e9ea57b32886aff0d7333849fb4f13ff
Maliha-Haroon <165473947+Maliha-Haroon@users.noreply.github.com>
2024-09-30 19:07:52 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,14 +1,32 @@
 "use client";
-import React from 'react'
-import Link from 'next/link' ;
-import { useRouter } from 'next/navigation';
 
-function navbar() {
-    const router = useRouter();
+import React from 'react';
+import { usePathname, useRouter } from 'next/navigation'; // Importing usePathname and useRouter from next/navigation
+import Link from 'next/link';
+
+export default function Home() {
+  const router = useRouter(); // Use the router from next/navigation
+
   return (
-    <div><Link href = "/navbar">Welcome to NAVBAR</Link></div>
-    
+    <>
+      <h1>Home Page</h1>
+      <h2>Link</h2>
+      <Link href="/about">Go to About Page</Link>
+      <br />
+      <Link href="/footer">Go to Footer</Link>
+      <br />
+      <Link href="/navbar">This is Navbar</Link>
+      <br />
+      <Link href="/contact">Contact Us</Link>
+
+      <h2>Navigation</h2>
+      <button onClick={() => router.push("/about")}><b><i>ABOUT</i></b></button>
+      <br />
+      <button onClick={() => router.push("/footer")}><b><i>FOOTER</i></b></button>
+      <br />
+      <button onClick={() => router.push("/navbar")}><b><i>NAVBAR</i></b></button>
+      <br />
+      <button onClick={() => router.push("/contact")}><b><i>CONTACT</i></b></button>
+    </>
   );
 }
-
-export default navbar;
